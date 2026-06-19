8499c551a7caa0f3ef603b802a825aa366001c5d
phuongnh <phuongnh@elcom.com>
2026-06-19 09:31:57 +0700

    fix(library): Enable Feign Clients on CrbtCommunityLibraryApplication

--- business-services/crbt-community-library/src/main/java/com/platform/crbtcommunitylibrary/CrbtCommunityLibraryApplication.java
+++ business-services/crbt-community-library/src/main/java/com/platform/crbtcommunitylibrary/CrbtCommunityLibraryApplication.java
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
 @SpringBootApplication(scanBasePackages = {"com.platform.crbtcommunitylibrary", "com.platform.common"})
 @EnableCaching
+@EnableFeignClients
 public class CrbtCommunityLibraryApplication {
 
     public static void main(String[] args) {
